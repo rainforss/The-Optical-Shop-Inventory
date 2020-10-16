4ab8ed8a6a63529afc378aa46f323dd66a53aa51
rainforss <shengyan@ualberta.ca>
2020-10-15 19:17:39 -0600

    Fixed GET_ITEMS in item reducer

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -12,7 +12,7 @@ export default function (state = initialState, action) {
     case types.GET_ITEMS:
       return {
         ...state,
-        items: [...state.items, ...action.items],
+        items: [...action.items],
         loading: false,
         totalCount: action.count,
       };
